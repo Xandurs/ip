02ea6d7474616aaee985635f0dad97760c98cdd3
Xandurs <xanderlim@live.com.sg>
2026-08-20 17:54:14 +0800

    level-0: rename chatbot to Joey. add greetings and exit

---
+++ src/main/java/Joey.java
@@ -0,0 +1,19 @@
+public class Joey {
+    public static void main(String[] args) {
+        String banner = "     _  ___  _______   __\n"
+                + "    | |/ _ \\| ____\\ \\ / /\n"
+                + " _  | | | | |  _|  \\ V / \n"
+                + "| |_| | |_| | |___  | |  \n"
+                + " \\___/ \\___/|_____| |_|  \n";
+
+        String line = "----------------------------------------";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Joey");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+    
+}
