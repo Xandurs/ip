9a1c37d4065acb015b1cddafd0ab20281334ef88
Xandurs <xanderlim@live.com.sg>
2026-08-27 17:24:20 +0800

    Added Echo feature to Joey

--- src/main/java/Joey.java
+++ src/main/java/Joey.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Joey {
     public static void main(String[] args) {
         String banner = "     _  ___  _______   __\n"
@@ -7,13 +8,28 @@ public class Joey {
                 + " \\___/ \\___/|_____| |_|  \n";
 
         String line = "----------------------------------------";
+        Scanner in = new Scanner(System.in); //build a scanner object that scans and read from system.in (keyboard)
+        
         System.out.println(line);
         System.out.println(banner);
         System.out.println("Hello! I'm Joey");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while(true){
+            String command = in.nextLine().trim();
+            if(command.equalsIgnoreCase("bye")){
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+
+            }
+            System.out.println(line);
+            System.out.println(" " + command);
+            System.out.println(line);
+        }
+        
     }
     
 }
